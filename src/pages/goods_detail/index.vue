e1b2ec9fa87ef3570360733bf7fc2241f06f1d32
Eysen-ZZT <2465597618@qq.com>
2020-09-24 21:27:42 +0800

    完成商品详情页轮播图 以及大图预览功能

--- src/pages/goods_detail/index.vue
+++ src/pages/goods_detail/index.vue
@@ -1,49 +1,88 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="content">
+        <swiper
+            class="swiper"
+            indicator-dots
+            duration="500"
+            indicator-color="#fff"
+            indicator-active-color="#FF2262"
+        >
+            <swiper-item
+                v-for="(item, index) in goodsData.pics"
+                :key="item.pics_id"
+                class="swiper-item"
+            >
+                <image
+                    :src="item.pics_big"
+                    mode="widthFix"
+                    class="swiper-item-pic"
+                    @tap="showBigImg(index)"
+                />
+            </swiper-item>
+        </swiper>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+    data() {
+        return {
+            // 商品详情数据
+            goodsData: {},
+            // 商品id
+            goods_id: "",
+            // 预览大图数据
+            picsUrl: [],
+        }
+    },
+    onLoad(obj) {
+        this.goods_id = obj.goods_id
+        this.getGoodsDetail()
+    },
+    methods: {
+        // 获取商品详情数据
+        getGoodsDetail() {
+            uni.request({
+                url:
+                    "https://api-hmugo-web.itheima.net/api/public/v1/goods/detail", //仅为示例，并非真实接口地址。
+                data: {
+                    goods_id: this.goods_id,
+                },
+                success: (res) => {
+                    console.log(res)
+                    const goodsData = res.data.message
+                    this.goodsData = goodsData
+                    // 提取预览图片地址
+                    this.picsUrl = goodsData.pics.map((item) => {
+                        return item.pics_big
+                    })
+                },
+            })
+        },
+        // 预览大图
+        showBigImg(index) {
+            uni.previewImage({
+                urls: this.picsUrl,
+                current: this.picsUrl[index],
+            })
+        },
+    },
+}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang='scss'>
+.content {
+    width: 750rpx;
+    .swiper {
+        height: 750rpx;
+        .swiper-item {
+            display: flex;
+            justify-content: center;
+            .swiper-item-pic {
+                width: 750rpx;
+                height: 750rpx;
+            }
+        }
+    }
+}
 </style>
--- src/pages/goods_list/index.vue
+++ src/pages/goods_list/index.vue
@@ -4,24 +4,54 @@
         <view class="goods">
             <view class="options">
                 <view
-                    :class="{'list':true,'active':current==index}"
-                    v-for="(item,index) in options"
+                    :class="{ list: true, active: current == index }"
+                    v-for="(item, index) in options"
                     :key="index"
                     @click="handleClass(index)"
-                >{{item}}</view>
+                    >{{ item }}</view
+                >
             </view>
-            <scroll-view scroll-y class="goods-item-list">
-                <view class="goods-item" v-for="item in productData" :key="item.goods_id">
-                    <navigator url navigator hover-class="className" class="navigator">
+            <scroll-view
+                scroll-y
+                class="goods-item-list"
+                @scrolltolower="handleLoad"
+            >
+                <view
+                    class="goods-item"
+                    v-for="item in productData"
+                    :key="item.goods_id"
+                >
+                    <navigator
+                        :url="`/pages/goods_detail/index?goods_id=${item.goods_id}`"
+                        navigator
+                        hover-class="className"
+                        class="navigator"
+                    >
                         <view class="goods-pic">
-                            <image :src="item.goods_small_logo" mode />
+                            <!-- 
+                                解决无图片的三种方案：
+                                1.v-if 
+                                2.三元表达式
+                                3.短路运算
+                            -->
+                            <image
+                                :src="
+                                    item.goods_small_logo || '/static/empty.png'
+                                "
+                                mode
+                            />
                         </view>
                         <view class="goods-info">
-                            <view class="goods-name">{{item.goods_name}}</view>
-                            <view class="goods-price">￥{{item.goods_price}}</view>
+                            <view class="goods-name">{{
+                                item.goods_name
+                            }}</view>
+                            <view class="goods-price">{{
+                                item.goods_price
+                            }}</view>
                         </view>
                     </navigator>
                 </view>
+                <u-loadmore :status="status" :load-text="loadText" />
             </scroll-view>
         </view>
     </view>
@@ -35,6 +65,8 @@ export default {
     },
     data() {
         return {
+            // 分类id
+            cid: "",
             // 排序选项
             options: ["综合", "销量", "价格"],
             // 控制选项类名
@@ -45,25 +77,57 @@ export default {
             pagesize: 10,
             // 商品列表数据
             productData: [],
+            // 加载状态
+            status: "loadmore",
+            // 加载提示信息
+            loadText: {
+                loadmore: "轻轻上拉",
+                loading: "努力加载中",
+                nomore: "实在没有了",
+            },
+            // 判断是否有更多数据
+            hasMore: true,
         }
     },
     onLoad(obj) {
         // obj是从上一页面跳转传递过来的参数
-        this.getProductList(obj.cid)
+        this.cid = obj.cid
+        this.getProductList()
+    },
+    // 监听该页面用户下拉刷新事件
+    onPullDownRefresh() {
+        // 重置数据并发送请求重新获取数据
+        this.productData = []
+        this.pagenum = 1
+        this.hasMore = true
+        this.getProductList()
     },
     methods: {
-        getProductList(cid) {
+        // 获取商品列表数据
+        getProductList() {
+            if (!this.hasMore) return
+            this.status = "loading"
             uni.request({
                 url:
                     "https://api-hmugo-web.itheima.net/api/public/v1/goods/search", //仅为示例，并非真实接口地址。
                 data: {
-                    cid,
+                    cid: this.cid,
                     pagenum: this.pagenum,
                     pagesize: this.pagesize,
                 },
                 success: (res) => {
                     console.log(res)
-                    this.productData = res.data.message.goods
+                    const { goods } = res.data.message
+                    // 使用ES6拓展运算符将分页数据添加到列表
+                    this.productData = [...this.productData, ...goods]
+                    if (goods.length <= 0) {
+                        this.hasMore = false
+                        this.status = "nomore"
+                    } else {
+                        this.status = "loading"
+                    }
+                    // 请求回数据之后停止当前页面下拉刷新
+                    uni.stopPullDownRefresh()
                 },
             })
         },
@@ -71,11 +135,17 @@ export default {
         handleClass(index) {
             this.current = index
         },
+        // 滑到底部触发事件
+        handleLoad() {
+            // 当前页递增，发送请求，增加列表数据
+            this.pagenum++
+            this.getProductList()
+        },
     },
 }
 </script>
 
-<style lang="less">
+<style lang="scss">
 .content {
     width: 750rpx;
     .goods {
@@ -91,14 +161,16 @@ export default {
                 height: 88rpx;
                 line-height: 88rpx;
                 border-bottom: 3px solid transparent;
-                box-sizing: content-box;
+                box-sizing: border-box;
                 font-size: 28rpx;
             }
             .active {
                 border-bottom-color: #ff2262;
+                color: #ff2262;
             }
         }
         .goods-item-list {
+            height: calc(100vh - 88rpx);
             .goods-item {
                 padding: 0 26rpx;
                 .navigator {
@@ -106,6 +178,7 @@ export default {
                     .goods-pic {
                         width: 191rpx;
                         height: 191rpx;
+                        box-sizing: content-box;
                         padding: 9rpx 30rpx 9rpx 15rpx;
                         image {
                             width: 191rpx;
@@ -120,12 +193,21 @@ export default {
                         flex-direction: column;
                         justify-content: space-around;
                         .goods-name {
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            display: -webkit-box;
+                            -webkit-line-clamp: 2;
+                            -webkit-box-orient: vertical;
                             font-size: 22rpx;
                             color: #434343;
                         }
                         .goods-price {
                             font-size: 24rpx;
                             color: #ff2262;
+                            &::before {
+                                content: "￥";
+                                font-size: 20rpx;
+                            }
                         }
                     }
                 }
